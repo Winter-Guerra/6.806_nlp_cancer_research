16786bb526beb7e528a0e0089c1730382b7f8604
Winter Guerra <winterg@mit.edu>
2015-07-16 10:41:32 -0400

    Working on classifying the snippets.

--- shovel/vectorClassifier.py
+++ shovel/vectorClassifier.py
@@ -17,13 +17,17 @@ except ImportError:
 
 # import gensim
 from gensim import corpora, similarities, models
+import sklearn
+from sklearn.feature_extraction.text import CountVectorizer
+form sklearn import svm
+import numpy as np
 
 ######### CONFIGURATION
-d = 200 # The dimensionality of the thing
+d = 200 # The dimensionality of the vectors
 
 
 @task
-def analyzeCorpus(loadSavedData = False):
+def analyzeCorpus(loadSavedData = True):
 
 	corpusFilename = './training_data/corpus.txt'
 	modelFilename = './training_data/corpus.model'
@@ -70,8 +74,6 @@ def analyzeCorpus(loadSavedData = False):
 
 def makeDocumentVectors():
 
-
-
 	# Import word vector model
 	modelFilename = './training_data/corpus.model'
 	model = models.Doc2Vec.load(modelFilename)
@@ -108,5 +110,77 @@ def makeDocumentVectors():
 
 	return (X)
 
-def trainClassifier(documentVectors):
-	''' This uses document vectors to train a scikit classifier. '''
+@task
+def runSVMClassifier():
+
+	# Instantiate the scikit unigram/bigram vectorizer
+	# Note: This vectorizer expects the sentence to be tokenized
+	bigram_vectorizer = CountVectorizer(ngram_range=(1, 2), binary=True, analyzer=str.split)
+
+	############
+	# Train the bigram vectorizer
+	############
+
+	sentences = []
+	with open('./training_data/corpus.txt') as f:
+		sentences = f.read().split('\n')
+
+	bigram_vectorizer.fit(sentences)
+
+	###########
+	# Train SVM
+	###########
+
+	# Load the corpus. Each line in the corpus is a new snippet.
+	sentences = []
+	with open('./training_data/trainingSet.txt') as f:
+		sentences = f.read().split('\n')
+
+	# Make the feature vectors
+	X = bigram_vectorizer.transform(sentences)
+
+	# Import the training lables for the training set
+	y = []
+	with open('./training_data/trainingLabels.txt') as f:
+		y = np.array([ int(lable) for lable in f.read().split('\n')])
+
+	# Train the SVM
+	classifier = svm.SVC()
+	classifier.fit(X, y)
+
+	######################
+	# Find training error
+	######################
+	# Classify test set using SVM
+	y_predicted = classifier.predict(X)
+
+	# Calculate the classifier accuracy on the test set
+	diffArray = (y_predicted == y)
+	totalRight = sum(diffArray)
+	accuracy = totalRight/float(len(totalRight))
+	print("Training accuracy", accuracy)
+
+	######################
+	# Use the trained SVM to classify the test set
+	######################
+
+	# Get test set
+	sentences = []
+	with open('./training_data/testSet.txt') as f:
+		sentences = f.read().split('\n')
+
+	X = bigram_vectorizer.transform(sentences)
+
+	# GEt test set lables
+	y = []
+	with open('./training_data/testLables.txt') as f:
+		y = np.array([ int(lable) for lable in f.read().split('\n')])
+
+	# Classify test set using SVM
+	y_predicted = classifier.predict(X)
+
+	# Calculate the classifier accuracy on the test set
+	diffArray = (y_predicted == y)
+	totalRight = sum(diffArray)
+	accuracy = totalRight/float(len(totalRight))
+	print("Test accuracy", accuracy)
